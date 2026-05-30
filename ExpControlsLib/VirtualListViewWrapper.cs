a01cd6f89499b9e3f53a155aadcbbd8e3f60aa33
spam0115 <79288532+spam0115@users.noreply.github.com>
2026-05-30 17:40:19 -0500

    prepare for virtual listview wrapper

---
+++ ExpControlsLib/VirtualListViewWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExpControlsLib
+{
+    internal class VirtualListViewWrapper
+    {
+        private ListView _listView;
+
+        public VirtualListViewWrapper(ListView listView)
+        {
+            _listView = listView;
+        }
+    }
+}
